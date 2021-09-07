ccbc4502734d6f3d663afcb0b9eb7d29aa8333a4
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-09-07 23:57:12 +0530

    title

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ const loadData = () => {
 const displayBook = data => {
 
     // result found section 
-
     const resultContainer = document.getElementById('result-container');
     const resultFound = document.createElement('div');
     resultContainer.innerHTML = `
@@ -21,16 +20,14 @@ const displayBook = data => {
     `
     resultContainer.appendChild(resultFound);
 
-
     // books result section
     const booksContainer = document.getElementById('books-container');
-    console.log(booksContainer);
+    booksContainer.textContent = '';
     const books = data.docs;
     books.forEach(book => {
-        console.log(book);
+        // console.log(book);
         const div = document.createElement('div');
         div.classList.add('col');
-        console.log(book.cover_i);
         let imgUrl;
         if (!book.cover_i) {
             imgUrl = `https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132484366.jpg`;
@@ -38,16 +35,17 @@ const displayBook = data => {
         else {
             imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         }
-
+        console.log(book.author_name);
         div.innerHTML = `
         <div class="card h-100">
             <img src="${imgUrl}" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body">
-            <h5 class="card-title">by ${books.author_name}</h5>
-            <p class="card-text">xx</p>
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text">by ${book.author_name}</p>
         </div>
             <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+            <small class="text-muted">First publish year: ${book.first_publish_year}</small>
+            <small class="text-muted">Publish Date: ${book?.publish_date}</small>
             </div>
         </div>
         `
