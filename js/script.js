d29050604f0b688fb91b3fc96f3915fd06a9ab01
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-09-07 23:13:44 +0530

    add thumnail

--- js/script.js
+++ js/script.js
@@ -7,5 +7,50 @@ const loadData = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayBook(data));
+}
+
+const displayBook = data => {
+
+    // result found section 
+
+    const resultContainer = document.getElementById('result-container');
+    const resultFound = document.createElement('div');
+    resultContainer.innerHTML = `
+        <p>Result found: ${data.numFound}</p> 
+    `
+    resultContainer.appendChild(resultFound);
+
+
+    // books result section
+    const booksContainer = document.getElementById('books-container');
+    console.log(booksContainer);
+    const books = data.docs;
+    books.forEach(book => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        console.log(book.cover_i);
+        let imgUrl;
+        if (!book.cover_i) {
+            imgUrl = `https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132484366.jpg`;
+        }
+        else {
+            imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        }
+
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${imgUrl}" class="card-img-top w-50 mx-auto" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">by ${books.author_name}</h5>
+            <p class="card-text">xx</p>
+        </div>
+            <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        `
+        booksContainer.appendChild(div);
+    })
 }
